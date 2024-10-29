89fa8325199c73df3c7905226ff56fd302d8414b
GojoSatoru77866 <168193458+GojoSatoru77866@users.noreply.github.com>
2024-10-29 14:21:35 +0300

    Update الاشتراك_1.js

--- plugins/الاشتراك_1.js
+++ plugins/الاشتراك_1.js
@@ -16,7 +16,7 @@ const handler = async (m, { conn }) => {
 > Zenon↳🐢↲𝐁𝐎𝐓
 > 〔 الاشتراك المجاني┊ ˼‏ 🚀˹ ↶〕
 *⋅ ───━ •﹝👑﹞• ━─── ⋅*
-            *زينو🐢ــن بــ🤖ــوت*
+            *لينو بــ🤖ــوت*
 *⋅ ───━ •﹝👑﹞• ━─── ⋅*
 ╗───¤﹝السعر ↶ 💵﹞
 > •┊˹👾˼┊- رقم وهمي
@@ -35,7 +35,7 @@ const handler = async (m, { conn }) => {
 *ملحوظة : الاشتراك المجاني اسبوع واحد فقط وبمقابل 🚀*
 *⋅ ───━ •﹝👑﹞• ━─── ⋅*
 > 〔تـوقـيـع┊ ˼‏📜˹ 〕↶
-⌠Zenon↳🐢↲𝐁𝐎𝐓⌡
+⌠Dazai🖤⌡
 *⋅ ───━ •﹝👑﹞• ━─── ⋅*`,
                         subtitle: "Elsony"
                     },
@@ -47,11 +47,11 @@ const handler = async (m, { conn }) => {
                         buttons: [
                             {
                                 name: "cta_url",
-                                buttonParamsJson: '{"display_text":"⌈🚀╎𝐁𝐔𝐘 ˹💰˼ 𝐍𝐎𝐖╎🚀⌋","url":"https://api.whatsapp.com/send?phone=+201061419002","merchant_url":"https://api.whatsapp.com/send?phone=+201061419002"}'
+                                buttonParamsJson: '{"display_text":"⌈🚀╎𝐁𝐔𝐘 ˹💰˼ 𝐍𝐎𝐖╎🚀⌋","url":"https://api.whatsapp.com/send?phone=+201061419002","merchant_url":"https://api.whatsapp.com/send?phone=+967778668253"}'
                             },
                             {
                                 name: "cta_url",
-                                buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الـمـطـور╎📲⌋","url":"https://chat.whatsapp.com/BrQVKh6Kt0X8LuMVWzW9XR","merchant_url":"https://chat.whatsapp.com/BrQVKh6Kt0X8LuMVWzW9XR"}'
+                                buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الـمـطـور╎📲⌋","url":"https://chat.whatsapp.com/BrQVKh6Kt0X8LuMVWzW9XR","merchant_url":"https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V"}'
                             }
                         ]
                     }
